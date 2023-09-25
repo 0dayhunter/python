b41c2a484357be8bccc3cc2335e6db7f7ce4df87
0dayhunter <78475739+0dayhunter@users.noreply.github.com>
2023-09-25 13:36:36 +0530

    Create b4.py

---
+++ b4.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Define matrices A and B
+A = np.array([[1, 2, 3], [4, 5, 6]])
+B = np.array([[7, 8], [9, 10], [11, 12]])
+
+# Multiply the matrices
+result_matrix = np.dot(A, B)
+
+# Display the result
+print(result_matrix)
