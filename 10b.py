3914d408ab45c24be3d5b05c480605e910a00caf
0dayhunter <78475739+0dayhunter@users.noreply.github.com>
2023-09-29 20:06:14 +0530

    10b.py

--- 10b.py
+++ 10b.py
@@ -1,21 +1,24 @@
-import requests, json # importing requests and json
-BASE_URL = https://api.openweathermap.org/data/2.5/weather? # base URL
-CITY = "Hyderabad"
+import requests, json  # importing requests and json
 
-# API key API_KEY = "Your API Key"
-URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY # upadting the URL
-response = requests.get(URL) # HTTP request
-if response.status_code == 200: # checking the status code of the request
-data = response.json() # getting data in the json format
-main = data['main'] # getting the main dict block
-temperature = main['temp'] # getting temperature
-humidity = main['humidity'] # getting the humidity
-pressure = main['pressure'] # getting the pressure
-report = data['weather'] # weather report
-print(f"{CITY:-^30}")
-print(f"Temperature: {temperature}")
-print(f"Humidity: {humidity}")
-print(f"Pressure: {pressure}")
-print(f"Weather Report: {report[0]['description']}")
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"  # base URL
+CITY = "Bangalore"
+API_KEY = "6f4ba13edf60f5338d0078eb0717ae85"
+URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY  # updating the URL
+
+response = requests.get(URL)  # HTTP request
+
+if response.status_code == 200:  # checking the status code of the request
+    data = response.json()  # getting data in the json format
+    main = data['main']  # getting the main dict block
+    temperature = main['temp']  # getting temperature
+    humidity = main['humidity']  # getting the humidity
+    pressure = main['pressure']  # getting the pressure
+    report = data['weather'][0]['description']  # weather report
+
+    print(f"{CITY:-^30}")
+    print(f"Temperature: {temperature}")
+    print(f"Humidity: {humidity}")
+    print(f"Pressure: {pressure}")
+    print(f"Weather Report: {report}")
 else:
-print("Error in the HTTP request") 
+    print("Error in the HTTP request")
